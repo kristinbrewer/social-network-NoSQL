3c4e1c2ecc3e9b3e6c7471b28c3e793ca2098525
Kristin Brewer <kr527106@gmail.com>
2023-03-07 15:46:09 -0500

    added user.js model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'friend',
+            },
+    ],
+    },
+    //indicates we want virtuals to be included with response
+    {
+        toJSON: {
+          getters: true,
+        },
+     }
+);
+
+//virtual to retrieve the length of the user's friends 
+userSchema
+.virtual('friendCount')
+.get(function() {
+    return `this.friends.length`;
+})
+
+//initializes user model
+const User = model('user', userSchema);
+module.exports = User;
