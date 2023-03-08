285e7d199a112366cb4c2ea5d83186e675666f83
Kristin Brewer <kr527106@gmail.com>
2023-03-08 16:43:42 -0500

    initial routes added to usercontroller.js

---
+++ controllers/userController.js
@@ -0,0 +1,117 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+
+module.exports = {
+    //get all users 
+    getUsers(req, res) {
+        User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+            };
+            return res.json(userObj);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        })
+    },
+
+    //get single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .select('-__v')
+          .then(async (user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with that ID' })
+              : res.json({
+                  user,
+                })
+          )
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
+        // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+             .catch((err) => res.status(500).json(err));
+  },
+  //update user
+  updateSingleUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //delete user and their thoughts
+  deleteSingleUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : Thought.findOneAndUpdate(
+              { users: req.params.user },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: 'User deleted, but no thoughts found',
+            })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+//add friend
+addFriend(req, res) {
+    console.log('You are adding a friend');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+//delete friend 
+deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+};
