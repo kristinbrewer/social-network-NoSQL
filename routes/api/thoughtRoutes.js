167fa63c09ba0c9c3cc71fb413398a823e9c8abf
Kristin Brewer <kr527106@gmail.com>
2023-03-08 12:11:45 -0500

    added reaction api to thoughtroutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,8 @@ const {
     createThought,
     updateThought, 
     deleteThought,
+    addReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController.js');
 
 // /api/thoughts
@@ -17,5 +19,15 @@ router
 .get(getSingleThought)
 .put(updateThought)
 .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+.route('/:thoughtId/reactions')
+.post(addReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
 
 module.exports = router;
