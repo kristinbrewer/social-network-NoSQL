3503899bc53ee81c0de779631caa5aacd579e5fb
Kristin Brewer <kr527106@gmail.com>
2023-03-09 14:23:15 -0500

    updated user controller, server.js, established atlas/heroku connection

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,16 +6,8 @@ module.exports = {
     //get all users 
     getUsers(req, res) {
         User.find()
-        .then(async (users) => {
-            const userObj = {
-                users,
-            };
-            return res.json(userObj);
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-        })
+        .then((users) => res.json(userObj))
+        .catch((err) => res.status(500).json(err));
     },
 
     //get single user
